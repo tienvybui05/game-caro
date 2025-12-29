9f1f496c27f35de7ecfe11f3356df54f45a23471
tienvybui05 <tienvy27052005@gmail.com>
2025-12-29 13:46:01 +0700

    Initial commit - Spring Boot Caro WebSocket

---
+++ src/main/java/ut/edu/gamecaro/GameCaroApplication.java
@@ -0,0 +1,13 @@
+package ut.edu.gamecaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCaroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameCaroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ut/edu/gamecaro/GameCaroApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.edu.gamecaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCaroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
